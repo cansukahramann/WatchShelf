551f92780229152c38b7686c8f6bf13e364f1e8d
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2024-11-12 20:54:16 +0300

    Changed GenreCell background

--- Scenes/CategoriesScene/GenreCell.swift
+++ Scenes/CategoriesScene/GenreCell.swift
@@ -16,39 +16,13 @@ final class GenreCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        applyGradient()
+
         setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private func applyGradient() {
-        gradientLayer = CAGradientLayer()
-        gradientLayer?.colors = [UIColor.black.cgColor, UIColor.black.cgColor]
-        gradientLayer?.startPoint = CGPoint(x: 0.5, y: 0)
-        gradientLayer?.endPoint = CGPoint(x: 0.5, y: 1)
-        gradientLayer?.locations = [0.0, 1.0]
-        gradientLayer?.frame = contentView.bounds
-        contentView.layer.insertSublayer(gradientLayer!, at: 0)
-        
-        let borderLayer = CALayer()
-        borderLayer.frame = contentView.bounds
-        borderLayer.borderColor = UIColor.white.cgColor
-        borderLayer.borderWidth = 4.0
-        borderLayer.cornerRadius = 12.0
-        borderLayer.opacity = 0.5
-
-        borderLayer.shadowColor = UIColor.white.cgColor
-        borderLayer.shadowOpacity = 0.9
-        borderLayer.shadowOffset = CGSize(width: 8, height: 8)
-        borderLayer.shadowRadius = 10.0
-
-        contentView.layer.addSublayer(borderLayer)
-        
-    }
-
     
     func animateCellTap(animationCompletion: @escaping () -> Void) {
         UIView.animate(withDuration: 0.1) { [self] in
@@ -63,8 +37,10 @@ final class GenreCell: UICollectionViewCell {
     }
     
     private func setupUI() {
-        layer.cornerRadius = 15
-        layer.masksToBounds = true
+        contentView.layer.borderWidth = 4.0
+        contentView.layer.borderColor = UIColor.white.cgColor
+        contentView.layer.cornerRadius = 12.0
+        contentView.clipsToBounds = true
         contentView.addSubview(categoryName)
         
         NSLayoutConstraint.activate([
