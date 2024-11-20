9541865dc7358cbc627810de40f8f648d3fcfa12
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2024-11-20 23:41:30 +0300

    Removed DispatchGroup usage from TVShowDetailViewModel

--- Scenes/TVShowScene/TVShowDetailViewModel.swift
+++ Scenes/TVShowScene/TVShowDetailViewModel.swift
@@ -19,7 +19,6 @@ class TVShowDetailViewModel {
     var tvCastModel = [SeriesCast]()
     var tvVideoModel = [Results]()
     var tvSimilarModel = [SimilarResult]()
-    let group = DispatchGroup()
     weak var delegate: TVShowDetailViewModelDelegate!
     private let service: TVShowDetailService!
     var seriesID: Int
@@ -28,7 +27,6 @@ class TVShowDetailViewModel {
         self.service = service
         self.seriesID = seriesID
     }
-    
     
     var isFavorite: Bool {
         WatchListStore.shared.isMediaSaved(id: model.id)
