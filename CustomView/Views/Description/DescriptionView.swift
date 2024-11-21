5221c6f600ec63dcb7a70464ff944ea902215801
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2024-11-21 12:16:08 +0300

    Add 'Read More'/'Read Less' toggle for description label

--- CustomView/Views/Description/DescriptionView.swift
+++ CustomView/Views/Description/DescriptionView.swift
@@ -12,13 +12,23 @@ class DescriptionView: UIView {
     
     let label: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 0
+        label.numberOfLines = 2
         label.font = UIFont.preferredFont(forTextStyle: .body)
-        label.textAlignment = .justified
+        label.textAlignment = .natural
         label.clipsToBounds = true
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Read More", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var isExpanded = false
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,16 +41,34 @@ class DescriptionView: UIView {
     
     private func setupDescriptionLabel() {
         addSubview(label)
+        addSubview(button)
         NSLayoutConstraint.activate([
             label.topAnchor.constraint(equalTo: topAnchor,constant: 12),
             label.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 12),
             label.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -12),
-            label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12)
+            
+            button.topAnchor.constraint(equalTo: label.bottomAnchor,constant: 8),
+            button.trailingAnchor.constraint(equalTo: label.trailingAnchor),
+            button.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
+        
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
     
     func configure(text: String) {
         label.text = text
+    }
+    
+    @objc
+    func didTapButton() {
+        isExpanded.toggle()
+        if isExpanded {
+            label.numberOfLines = 0
+            button.setTitle("Read Less", for: .normal)
+        } else {
+            label.numberOfLines = 2
+            button.setTitle("Read More", for: .normal)
+        }
     }
     
 }
