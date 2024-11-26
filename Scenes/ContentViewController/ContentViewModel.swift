1d9bce146b48d2194cd086ab41f0f492f11bfe0f
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2024-11-26 14:26:14 +0300

    Refactored ContentViewModel to use ContentServiceProtocol

--- Scenes/ContentViewController/ContentViewModel.swift
+++ Scenes/ContentViewController/ContentViewModel.swift
@@ -13,28 +13,34 @@ protocol ContentViewModelDelegate: AnyObject {
 }
 
 final class ContentViewModel {
-    
     weak var delegate: ContentViewModelDelegate?
-    private let service: ContentService
+    private let service: ContentServiceProtocol
     
     var allContentResults = [ContentResult]()
+    private var page = 1
+    private var shouldRequestNextPage = true
+    private var isFetchingContent = false
     
-    init(service: ContentService) {
+    init(service: ContentServiceProtocol) {
         self.service = service
     }
     
     func fetchAllContent() {
-        
-        service.loadContent { [weak self] result in
+        guard !isFetchingContent else { return }
+        isFetchingContent = true
+        service.fetchContent(requestModel: CommonRequestModel(page: page)) { [weak self] result in
             guard let self = self else { return }
             
             switch result {
             case .success(let results):
                 self.allContentResults.append(contentsOf: results)
+                self.page += 1
+                self.shouldRequestNextPage = !results.isEmpty
             case .failure(let error):
                 print("Error: \(error)")
             }
             delegate?.updateCollectionView()
+            isFetchingContent = false
         }
     }
 }
