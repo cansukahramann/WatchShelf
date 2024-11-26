c22df946e349d55aa812e796a1225f856d8d9634
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2024-11-26 14:25:20 +0300

    Added extension for Encodable to convert to dictionary

---
+++ Utilites/Encodable+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  Encodable+Ext.swift
+//  WatchShelf
+//
+//  Created by Cansu Kahraman on 25.11.2024.
+//
+
+import Foundation
+
+extension Encodable {
+    var asDictionary: [String: Any] {
+        guard let data = try? JSONEncoder().encode(self),
+              let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+            return [:]
+        }
+        
+        return json
+    }
+}
