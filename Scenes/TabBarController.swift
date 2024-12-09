787a07a1de77408d28a68ed0cd53e778d582ec46
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2024-12-09 16:10:25 +0300

    Refactored TabBarController by using Tab enum for tab bar item and titles

--- Scenes/TabBarController.swift
+++ Scenes/TabBarController.swift
@@ -7,8 +7,46 @@
 
 import UIKit
 
-final class TabBarController: UITabBarController {
+fileprivate enum Tab {
+    case movie
+    case tvShow
+    case categories
+    case search
+    case watchList
+    
+    var tabBarItem: UITabBarItem {
+        switch self {
+        case .movie:
+            UITabBarItem(title: "Movie", image: UIImage(systemName: "movieclapper"), tag: 0)
+        case .tvShow:
+            UITabBarItem(title: "Tv Show", image: UIImage(systemName: "play.tv"), tag: 1)
+        case .categories:
+           UITabBarItem(title: "Category", image: UIImage(systemName: "square.grid.2x2.fill"), tag: 2)
+        case .search:
+            UITabBarItem(title: "Search", image: UIImage(systemName: "sparkle.magnifyingglass"), tag: 3)
+        case .watchList:
+            UITabBarItem(title: "Watch List", image: UIImage(systemName: "list.and.film"), tag: 4)
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .movie:
+            "Movies"
+        case .tvShow:
+            "Tv Show"
+        case .categories:
+            "Categories"
+        case .search:
+            "Search"
+        case .watchList:
+            "Watch List"
+        }
+    }
+}
 
+final class TabBarController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,36 +62,36 @@ final class TabBarController: UITabBarController {
 private extension TabBarController {
     func createMovieNC() -> UINavigationController {
         let movieVC = MovieViewController()
-        movieVC.title = "Movies"
-        movieVC.tabBarItem = UITabBarItem(title: "Movie", image: UIImage(systemName: "movieclapper"), tag: 0)
+        movieVC.title = Tab.movie.title
+        movieVC.tabBarItem = Tab.movie.tabBarItem
         return UINavigationController(rootViewController: movieVC)
     }
     
     func createTvShowNC() -> UINavigationController {
         let tvShowVC = TvShowViewController()
-        tvShowVC.title = "Tv Show"
-        tvShowVC.tabBarItem = UITabBarItem(title: "Tv Show", image: UIImage(systemName: "play.tv"), tag: 1)
+        tvShowVC.title = Tab.tvShow.title
+        tvShowVC.tabBarItem = Tab.tvShow.tabBarItem
         return UINavigationController(rootViewController: tvShowVC)
     }
     
     func createCategoryNC() -> UINavigationController {
         let categoryVC = GenreFactory.makeGenreFactory()
-        categoryVC.title = "Categories"
-        categoryVC.tabBarItem = UITabBarItem(title: "Category", image: UIImage(systemName: "square.grid.2x2.fill"), tag: 2)
+        categoryVC.title = Tab.categories.title
+        categoryVC.tabBarItem = Tab.categories.tabBarItem
         return UINavigationController(rootViewController: categoryVC)
     }
     
     func createSearchNC() -> UINavigationController {
         let searchVC = TrendViewController()
-        searchVC.title = "Search"
-        searchVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "sparkle.magnifyingglass"), tag: 3)
+        searchVC.title = Tab.search.title
+        searchVC.tabBarItem = Tab.search.tabBarItem
         return UINavigationController(rootViewController: searchVC)
     }
     
     func createWatchListNC() -> UINavigationController {
         let watchListVC = WatchListViewController()
-        watchListVC.title = "Watch List"
-        watchListVC.tabBarItem = UITabBarItem(title: "Watch List", image: UIImage(systemName: "list.and.film"), tag: 4)
+        watchListVC.title = Tab.watchList.title
+        watchListVC.tabBarItem = Tab.watchList.tabBarItem
         return UINavigationController(rootViewController: watchListVC)
     }
 }
